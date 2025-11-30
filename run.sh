e4df3ae4c15eaf0243f4fb2e10c9a4f1c558ee80
REJITH RAGHAVAN <rejithraghavan@REJITHs-MacBook-Air.local>
2025-11-29 18:31:05 -0800

    new

--- run.sh
+++ run.sh
@@ -17,9 +17,9 @@ cat data/batch_1.json | kcat -b kafkabroker -t stock_ticks -P
 
 docker exec -it hadoop hadoop fs -chmod -R 777 /
 
-# docker exec -it spark_test spark-submit --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
-#   --conf spark.kryo.registrator=org.apache.spark.HoodieSparkKryoRegistrar \
-#   --conf spark.sql.extensions=org.apache.spark.sql.hudi.HoodieSparkSessionExtension \
-#   --conf spark.sql.catalog.spark_catalog=org.apache.spark.sql.hudi.catalog.HoodieCatalog \
-#    --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0,org.apache.spark:spark-avro_2.12:3.5.0,org.apache.hudi:hudi-spark3.5-bundle_2.12:1.0.1 \
-#    /tmp/kafka_to_hudi.py
+docker exec -it spark_test spark-submit --conf spark.serializer=org.apache.spark.serializer.KryoSerializer \
+  --conf spark.kryo.registrator=org.apache.spark.HoodieSparkKryoRegistrar \
+  --conf spark.sql.extensions=org.apache.spark.sql.hudi.HoodieSparkSessionExtension \
+  --conf spark.sql.catalog.spark_catalog=org.apache.spark.sql.hudi.catalog.HoodieCatalog \
+   --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0,org.apache.spark:spark-avro_2.12:3.5.0,org.apache.hudi:hudi-spark3.5-bundle_2.12:1.0.1 \
+   /tmp/kafka_to_hudi_stock_prices.py
